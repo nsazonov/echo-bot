6350babf42231d6da71aa5431c27b1fab724f87e
nsazonov <perdelers@gmail.com>
2020-11-23 20:26:03 -0800

    Create a loop and getUpdates incrementing offset

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,27 @@
 module Main where
-
-import System.Environment
+import           System.Environment
+import qualified API.Telegram                  as TG
+import qualified API.Network                   as N
 
 main :: IO ()
 main = do
-    args <- getArgs
-    if null args then putStrLn "Usage:"
-                 else print (head args)    
+  args <- getArgs
+  if null args then putStrLn "Usage:" else print (head args)
+
+botLoop :: N.Token -> Maybe Integer -> IO ()
+botLoop token offset = do
+  putStrLn "Waiting..."
+  response <- N.runGetUpdate $ N.getUpdatesRequest token offset (Just 5)
+  case response of
+    Nothing -> do 
+      putStrLn "Nothing"      
+      botLoop token offset
+    (Just updates) -> do
+      case N.latest updates of
+        Nothing -> do 
+          putStrLn "No updates!"          
+          botLoop token offset
+        Just newOffset -> do 
+          putStrLn $ "Offset: " ++ show newOffset
+          botLoop token (Just (newOffset + 1))
+  
--- src/API/Network.hs
+++ src/API/Network.hs
@@ -4,12 +4,15 @@ module API.Network
   , runGetUpdate
   , runSendMessage
   , answerCallback
+  , latest
   , Timeout
+  , Token
   )
 where
 
-import           API.Telegram
+import qualified API.Telegram                  as TG
 import qualified Data.ByteString.Char8         as BC
+import           Data.List
 import           Network.HTTP.Simple
 
 telegramHost :: BC.ByteString
@@ -29,32 +32,46 @@ buildRequest token requestMethod host method =
     $ setRequestSecure True
     $ setRequestPort 443 defaultRequest
 
-getUpdatesRequest :: Token -> Maybe Timeout -> Request
-getUpdatesRequest token timeout =
+getUpdatesRequest :: Token -> Maybe Integer -> Maybe Timeout -> Request
+getUpdatesRequest token offset timeout =
   let request = buildRequest token "GET" telegramHost "getUpdates"
   in  setRequestQueryString
-        (maybe [] (\t -> [("timeout", Just (BC.pack $ show t))]) timeout)
+        (maybe
+          []
+          (\t ->
+            [ ("timeout", Just (BC.pack $ show t))
+            , ("offset" , fmap (BC.pack . show) offset)
+            ]
+          )
+          timeout
+        ) -- TODO: this is very ugly, consider creating ad-hoc type with moniid properties and toQueryString func
         request
 
-sendMessageRequest :: Token -> OutgoingMessage -> Request
+sendMessageRequest :: Token -> TG.OutgoingMessage -> Request
 sendMessageRequest token m =
   setRequestBodyJSON m $ buildRequest token "POST" telegramHost "sendMessage"
 
-answerCallback :: Token -> CallbackAnswer -> Request
-answerCallback token c = setRequestBodyJSON c $ buildRequest token "POST" telegramHost "answerCallbackQuery"
+answerCallback :: Token -> TG.CallbackAnswer -> Request
+answerCallback token c = setRequestBodyJSON c
+  $ buildRequest token "POST" telegramHost "answerCallbackQuery"
 
-runGetUpdate :: Request -> IO (Maybe Update)
+runGetUpdate :: Request -> IO (Maybe [TG.Update])
 runGetUpdate request = do
-    response <-
-      httpJSONEither request :: IO
-        (Response (Either JSONException GetUpdatesResponse))
-    case getResponseBody response of
-      (Left  _             ) -> return Nothing
-      (Right updateResponse) -> return $ safeHead $ guResult updateResponse
-  
-runSendMessage :: Request -> IO (Response Message)
+  response <-
+    httpJSONEither request :: IO
+      (Response (Either JSONException TG.GetUpdatesResponse))
+  case getResponseBody response of
+    (Left  _             ) -> return Nothing
+    (Right updateResponse) -> return $ Just $ TG.guResult updateResponse
+
+runSendMessage :: Request -> IO (Response TG.Message)
 runSendMessage = httpJSON
 
 safeHead :: [a] -> Maybe a
 safeHead []      = Nothing
 safeHead (x : _) = Just x
+
+-- TODO: the order should be desc
+-- TODO: return update, not the id
+latest :: [TG.Update] -> Maybe Integer
+latest = safeHead . sort . map TG.uId
