a7a841e20ee45e30d7b6efc27b719576687edea1
nsazonov <perdelers@gmail.com>
2020-11-24 19:44:47 -0800

    Implement Logger

--- src/Logger.hs
+++ src/Logger.hs
@@ -1,35 +1,38 @@
 module Logger
-  ( Level
-  , Config(..)
-  , withHandle
-  , log
-  , debug
-  , info
-  , warning
-  , error
+  ( Level (..),
+    Config (..),
+    withHandle,
+    log,
+    debug,
+    info,
+    warning,
+    error,
   )
 where
 
-import           Prelude                 hiding ( error
-                                                , log
-                                                )
+import Prelude hiding
+  ( error,
+    log,
+  )
 
 data Level = Debug | Info | Warning | Error deriving (Eq, Ord, Show)
-newtype Config = Config {level :: Level} deriving (Show)
-newtype Handle = Handle {config :: Config} deriving (Show)
+
+newtype Config = Config {cLevel :: Level} deriving (Show)
+
+newtype Handle = Handle {hConfig :: Config} deriving (Show)
 
 withHandle :: Config -> (Handle -> IO a) -> IO a
-withHandle = undefined
+withHandle c f = f Handle {hConfig = c}
 
 log :: Handle -> Level -> String -> IO ()
 log Handle {..} v x
-  | v >= verbosity = putStrLn $ "[" ++ show v ++ "]:" ++ show x
-  | otherwise      = return ()
-  where verbosity = Debug
+  | v >= verbosity = putStrLn $ "[" ++ show v ++ "]:" ++ x
+  | otherwise = return ()
+  where
+    verbosity = Debug
 
 debug, info, warning, error :: Handle -> String -> IO ()
 debug h = log h Debug
 info h = log h Info
 warning h = log h Warning
 error h = log h Error
-
