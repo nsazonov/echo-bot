8203652f381a494ffd2731a0166b0b33a0b4d7c5
nsazonov <perdelers@gmail.com>
2021-12-28 19:12:30 -0800

    Refactor executeCommand function

--- src/Bot.hs
+++ src/Bot.hs
@@ -48,34 +48,34 @@ type ClientSettings = Map.Map Target Int -- TODO: rename this
 
 data Messenger = VK | Telegram | Test deriving (Show)
 
-class Client a where
-  executeCommand :: a -> BotConfig -> Logger.Handle -> ClientSettings -> Target -> Command -> IO ClientSettings
-
-instance Client Messenger where
-  executeCommand :: Messenger -> BotConfig -> Logger.Handle -> ClientSettings -> Target -> Command -> IO ClientSettings
-  executeCommand VK _ _ _ _ _ = undefined
-  executeCommand Test _ _ _ _ _ = undefined
-  executeCommand Telegram config logger settings target (RepeatAnswer queryId repeatNumber) = do
-    let token = cToken config
-    let newSetting = Map.insert target repeatNumber settings
-    _ <- Telegram.sendAnswer token logger queryId repeatNumber -- TODO: log errors
-    return newSetting
-  executeCommand Telegram config logger settings target Repeat = do
-    let token = cToken config
-    let repeatNumber = Map.findWithDefault (cDefaultRepeatNumber config) target settings 
-    let statusMessage = "Current repeat number is " ++ show repeatNumber
-    _ <- Telegram.sendRepeatInput token logger target $ T.pack statusMessage -- TODO: log errors
-    return settings
-  executeCommand Telegram config logger settings target Help = do
-    let token = cToken config
-    let greetingsMessage = cGreetings config
-    _ <- Telegram.sendText token logger target greetingsMessage -- TODO: log errors
-    return settings
-  executeCommand Telegram config logger settings target (Action t) = do
-    let token = cToken config
-    let times = cDefaultRepeatNumber config
-    replicateM_ times (Telegram.sendText token logger target t) -- TODO: log failed messages here
-    return settings
+executeCommand ::
+  BotConfig ->
+  Logger.Handle ->
+  ClientSettings ->
+  Target ->
+  Command ->
+  IO ClientSettings
+executeCommand config logger settings target (RepeatAnswer queryId repeatNumber) = do
+  let token = cToken config
+  let newSetting = Map.insert target repeatNumber settings
+  _ <- Telegram.sendAnswer token logger queryId repeatNumber -- TODO: log errors
+  return newSetting
+executeCommand config logger settings target Repeat = do
+  let token = cToken config
+  let repeatNumber = Map.findWithDefault (cDefaultRepeatNumber config) target settings
+  let statusMessage = "Current repeat number is " ++ show repeatNumber
+  _ <- Telegram.sendRepeatInput token logger target $ T.pack statusMessage -- TODO: log errors
+  return settings
+executeCommand config logger settings target Help = do
+  let token = cToken config
+  let greetingsMessage = cGreetings config
+  _ <- Telegram.sendText token logger target greetingsMessage -- TODO: log errors
+  return settings
+executeCommand config logger settings target (Action t) = do
+  let token = cToken config
+  let times = cDefaultRepeatNumber config
+  replicateM_ times (Telegram.sendText token logger target t) -- TODO: log failed messages here
+  return settings
 
 runLoop :: BotConfig -> Logger.Handle -> IO ()
 runLoop config logger = botLoop config logger Map.empty Nothing
@@ -113,6 +113,6 @@ processUpdates config logger settings updates = do
   let others = mapMaybe fromMessage updates
   Logger.debug logger (("Number of callbacks: " ++ show (length callBacks)) :: String)
   Logger.debug logger (("Number of other commands: " ++ show (length others)) :: String)
-  mapM_ (uncurry (executeCommand (cMessenger config) config logger settings)) $ callBacks ++ others
+  mapM_ (uncurry (executeCommand config logger settings)) $ callBacks ++ others
   Logger.debug logger ("Finished processing the updates" :: String)
   return $ (,Map.empty) <$> TG.nextOffset updates
